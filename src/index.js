cfb03c108a7a49001058e02d47692933174afd96
Evan Hahn <me@evanhahn.com>
2017-01-28 22:11:44 +0000

    Generating potion names

--- src/index.js
+++ src/index.js
@@ -1,1 +1,79 @@
-console.log('hello world')
+/*
+ * A = adjectives
+ * P = potion types
+ * T = properties
+ * O = owner (proper name)
+ *
+ * a = <canvas>
+ * b = <body>
+ * c = canvas context
+ * d = document
+ * r = render function
+ * s = sample from an array (which can return undefined)
+ * t = <h1> for potion title
+ */
+
+/* eslint-disable no-undef */
+
+// define constants
+
+A = ['vile']
+O = ['fartbane']
+P = ['elixir', 'potion', 'beverage', 'cure', 'tonic']
+T = ['speed', 'illness']
+
+// set up CSS and DOM
+
+b.innerHTML += '<h1 id=t></h1>'
+
+a.style.margin = '0 auto'
+b.style.cssText = 'background:#003;color:#fff;text-align:center'
+t.style.textTransform = 'uppercase'
+
+// sample from an array, or return undefined
+
+s = (arr) => arr[Math.floor(Math.random() * (arr.length + 1))]
+
+// define render function
+
+r = () => {
+  let result = ''
+
+  let type, owner, adjective, property
+
+  while (!type) { type = s(P) }
+
+  while (!owner && !adjective && !property) {
+    owner = s(O)
+    adjective = s(A)
+    property = s(T)
+  }
+
+  if (owner) {
+    result = owner + "'s "
+  }
+
+  if (property) {
+    if (adjective) {
+      property = adjective + ' ' + property
+    }
+
+    if (Math.random() < 0.5) {
+      result += type + ' of ' + property
+    } else {
+      result += property + ' ' + type
+    }
+  } else {
+    if (adjective) {
+      result += adjective + ' '
+    }
+    result += type
+  }
+
+  if (result === type) {
+    throw new Error('uhg')
+  }
+
+  t.innerHTML = result
+}
+r()
